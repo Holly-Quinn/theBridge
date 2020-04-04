723c8a8a7c0296d161b82f0b3e2878cc3f848775
theCannonFod <afoddering@gmail.com>
2020-04-04 18:08:43 +0100

    First Commit

---
+++ scripts/scripts.js
@@ -0,0 +1,191 @@
+		function initMap() {
+  			var bridge = {lat: -25.344, lng: 131.036};
+  			var map = new google.maps.Map(
+      		document.getElementById('map'), {zoom: 4, center: uluru});
+  			var marker = new google.maps.Marker({position: uluru, map: map});
+		}	function initMap() {
+	
+  	var bridge = {lat: 54.969618, lng: -1.607444};
+  	var map = new google.maps.Map(
+    document.getElementById('map'), {zoom: 17, center: bridge});
+  	var marker = new google.maps.Marker({position: bridge, map: map});
+	
+}		function show_alert(){
+			
+			var firstNameValue = document.getElementById("firstName").value
+			var lastNameValue = document.getElementById("lastName").value
+			var emailValue = document.getElementById("emailAddress").value
+
+			if (firstNameValue == "") {
+		
+				alert("Unable to submit form - the First Name Field is blank.")
+		
+			} else if (lastNameValue == "") {
+		
+				alert("Unable to submit form - the Last Name Field is blank.")
+		
+			} else if (emailValue == "") {
+		
+				alert("Unable to submit form - the Email Address Field is blank.")
+		
+			} else {
+		
+				alert("Thank You - Form Submitted.")
+				//alert(document.getElementById("firstName").value)
+		
+			}
+			
+
+		}		function show_alert(){
+			
+			var firstNameValue = document.getElementById("firstName").value
+			var lastNameValue = document.getElementById("lastName").value
+			var emailValue = document.getElementById("emailAddress").value
+
+			if (firstNameValue == "") {
+		
+				alert("Unable to submit form - the First Name Field is blank.")
+		
+			} else if (lastNameValue == "") {
+		
+				alert("Unable to submit form - the Last Name Field is blank.")
+		
+			} else if (emailValue == "") {
+		
+				alert("Unable to submit form - the Email Address Field is blank.")
+		
+			} else {
+		
+				alert("Thank You - Form Submitted.")
+				//alert(document.getElementById("firstName").value)
+		
+			}
+			
+
+		}		function show_alert(){
+			
+			var firstNameValue = document.getElementById("firstName").value
+			var lastNameValue = document.getElementById("lastName").value
+			var emailValue = document.getElementById("emailAddress").value
+
+			if (firstNameValue == "") {
+		
+				alert("Unable to submit form - the First Name Field is blank.")
+		
+			} else if (lastNameValue == "") {
+		
+				alert("Unable to submit form - the Last Name Field is blank.")
+		
+			} else if (emailValue == "") {
+		
+				alert("Unable to submit form - the Email Address Field is blank.")
+		
+			} else {
+		
+				alert("Thank You - Form Submitted.")
+				//alert(document.getElementById("firstName").value)
+		
+			}
+			
+
+		}		function show_alert(){
+			
+			var firstNameValue = document.getElementById("firstName").value
+			var lastNameValue = document.getElementById("lastName").value
+			var emailValue = document.getElementById("emailAddress").value
+
+			if (firstNameValue == "") {
+		
+				alert("Unable to submit form - the First Name Field is blank.")
+		
+			} else if (lastNameValue == "") {
+		
+				alert("Unable to submit form - the Last Name Field is blank.")
+		
+			} else if (emailValue == "") {
+		
+				alert("Unable to submit form - the Email Address Field is blank.")
+		
+			} else {
+		
+				alert("Thank You - Form Submitted.")
+				//alert(document.getElementById("firstName").value)
+		
+			}
+			
+
+		}		function show_alert(){
+			
+			var firstNameValue = document.getElementById("firstName").value
+			var lastNameValue = document.getElementById("lastName").value
+			var emailValue = document.getElementById("emailAddress").value
+
+			if (firstNameValue == "") {
+		
+				alert("Unable to submit form - the First Name Field is blank.")
+		
+			} else if (lastNameValue == "") {
+		
+				alert("Unable to submit form - the Last Name Field is blank.")
+		
+			} else if (emailValue == "") {
+		
+				alert("Unable to submit form - the Email Address Field is blank.")
+		
+			} else {
+		
+				alert("Thank You - Form Submitted.")
+				//alert(document.getElementById("firstName").value)
+		
+			}
+			
+
+		}		function show_alert(){
+			
+			var firstNameValue = document.getElementById("firstName").value
+			var lastNameValue = document.getElementById("lastName").value
+			var emailValue = document.getElementById("emailAddress").value
+
+			if (firstNameValue == "") {
+		
+				alert("Unable to submit form - the First Name Field is blank.")
+		
+			} else if (lastNameValue == "") {
+		
+				alert("Unable to submit form - the Last Name Field is blank.")
+		
+			} else if (emailValue == "") {
+		
+				alert("Unable to submit form - the Email Address Field is blank.")
+		
+			} else {
+		
+				alert("Thank You - Form Submitted.")
+				//alert(document.getElementById("firstName").value)
+		
+			}
+			
+
+		}// JavaScript Document
+
+function submitForm() {
+	
+	if (document.input.firstName.value == "") {
+		
+		alert("Unable to submit form - the First Name Field is blank.")
+		
+	} else if (document.input.lastName.value == "") {
+		
+		alert("Unable to submit form - the Last Name Field is blank.")
+		
+	} else if (document.input.emailAddress.value == "") {
+		
+		alert("Unable to submit form - the Email Address Field is blank.")
+		
+	} else {
+		
+		alert("Thank You - Form Submitted.")
+		
+	}
+	
+}
